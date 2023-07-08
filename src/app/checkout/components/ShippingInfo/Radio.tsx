d7e6916c05fd462e014fb357cb0d376902332678
Sam Chang <sam21323@gmail.com>
2023-07-08 09:53:27 +0800

    feat: Adding radio select

--- src/app/checkout/components/ShippingInfo/Radio.tsx
+++ src/app/checkout/components/ShippingInfo/Radio.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+'use client';
+import { useContext } from 'react';
+import { CheckoutContext } from '../../context/CheckoutContext';
 
 export default function Radio({ label }: { label: string }) {
+  const { userInput, setUserInput } = useContext(CheckoutContext);
+
+  function handleCheck(e: React.ChangeEvent<HTMLInputElement>) {
+    const newUserInput = { ...userInput };
+    newUserInput.delieverTime = e.target.value;
+    setUserInput(newUserInput);
+  }
+
   return (
     <div className='flex gap-[6px]'>
       <label className='text-[14px] leading-[26px]'>{label}</label>
-      <input type='radio' />
+      <input
+        type='radio'
+        name={label}
+        value={label}
+        onChange={(e) => handleCheck(e)}
+        checked={userInput.delieverTime === label}
+      />
     </div>
   );
 }

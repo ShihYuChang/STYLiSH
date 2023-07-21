12929600f866425bd586efa490d0a0b8efb168eb
Sam Chang <sam21323@gmail.com>
2023-07-21 21:12:15 +0800

    Adding basic sign in function

--- src/app/login/components/LoginBtn.tsx
+++ src/app/login/components/LoginBtn.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+'use client';
+import { useContext } from 'react';
 import { api } from '@/utils/api';
+import { Context } from '../context/Context';
 
 export default function LoginBtn() {
   const { signIn } = api;
+  const { userInput } = useContext(Context);
+
+  async function handleSignIn() {
+    const response = await signIn(userInput.userName, userInput.password);
+    console.log(response);
+  }
+
   return (
-    <button className='w-full xl:w-[240px] h-[44px] xl:h-[64px] bg-black text-white tracking-[4px]'>
+    <button
+      className='w-full xl:w-[240px] h-[44px] xl:h-[64px] bg-black text-white tracking-[4px]'
+      onClick={handleSignIn}
+    >
       登入
     </button>
   );
